692390a1804e82e890f0de3d4643ce4e33af5b59
Thai Nguyen <thai.nguyen@eastagile.com>
2015-06-05 23:35:59 +0700

    Remove angular route

--- app/app.js
+++ app/app.js
@@ -1,7 +1,6 @@
 var API_SERVER = 'https://young-beyond-8772.herokuapp.com/'
 var mortGageClub = angular.module('mortGageClub',
   [
-    'ngRoute',
     'ngResource',
     'ui.router',
     'sessions',
